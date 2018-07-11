725323f79f9dbfe37a03e6d5643feb1dcba54217
godkin1211 <jiajyun.ciou@gmail.com>
2018-07-11 16:31:59 +0800

    Add templates and edit myapp/routes.py

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8-*-
+from flask import render_template
 from myapp import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Godkin'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - My Flask App</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title = 'Home', user = user)
+
